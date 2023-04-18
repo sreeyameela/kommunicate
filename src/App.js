c2f01f4cc2e4fe10c3a496d0694bf657a3c2e326
Sreeya Meela <sreeyameela@Sreeyas-Air.lan>
2023-04-17 21:06:34 -0400

    kommunicate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+class KommunicateChat extends Component {
+   constructor(props){
+       super(props);
+   } 
+componentDidMount(){
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"31e86c3017597b6413fbbe3cf554d500e","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
-
-export default App;
+render(){
+   return(
+       <div>
+           <h1>CHATBOT</h1>
+     </div>
+   )
+}
+}
+export default KommunicateChat;
